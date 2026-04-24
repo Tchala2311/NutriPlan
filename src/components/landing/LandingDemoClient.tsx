85f1fd0306e72a118e2e1ca409cbe5f2a9773aaf
CTO <cto@paperclip.ing>
2026-04-24 21:48:06 +0300

    Revert "Revert "fix(landing): remove next/dynamic wrapper from LandingDemoClient""
    This reverts commit 67e6e5344f2661c0c5a9e2bd56893ab65bb765af.

--- src/components/landing/LandingDemoClient.tsx
+++ src/components/landing/LandingDemoClient.tsx
@@ -1,7 +1,2 @@
 "use client";
-import dynamic from "next/dynamic";
-
-export const LandingDemoClient = dynamic(
-  () => import("@/components/landing/LandingDemo").then((m) => ({ default: m.LandingDemo })),
-  { ssr: false }
-);
+export { LandingDemo as LandingDemoClient } from "@/components/landing/LandingDemo";
